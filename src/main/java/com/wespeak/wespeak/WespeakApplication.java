6ac7ac95eb06bd32172a9dfdeb484fd4112cf686
smallgigach <artyom.s.2020@gmail.com>
2025-08-21 23:47:40 +0500

    init project

---
+++ src/main/java/com/wespeak/wespeak/WespeakApplication.java
@@ -0,0 +1,14 @@
+package com.wespeak.wespeak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WespeakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WespeakApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/test/java/com/wespeak/wespeak/WespeakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wespeak.wespeak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WespeakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
