e98cc064aebfd248e956e2c2be0a127a90417c95
smallgigach <artyom.s.2020@gmail.com>
2025-08-22 19:55:11 +0500

    update: update gitignore

--- src/main/java/com/wespeak/wespeak/WespeakApplication.java
+++ src/main/java/com/wespeak/wespeak/WespeakApplication.java
@@ -9,6 +9,7 @@ public class WespeakApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WespeakApplication.class, args);
 		System.out.println("Hello world");
+		
 	}
 
 }
